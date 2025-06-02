c9e1ad197583321574085880324c91bb9689aed7
andvolveluca <109103844+andluca@users.noreply.github.com>
2025-06-02 17:39:42 -0300

    refactor: Simplify database connection function and update to use DATABASE_URL

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,8 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
 import os
+import psycopg2
 
-def get_connection():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        cursor_factory=RealDictCursor
-    )
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
