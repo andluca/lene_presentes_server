5b8619ecd28f95f70913e0f5e3d8de8d1354ed5f
andvolveluca <109103844+andluca@users.noreply.github.com>
2025-06-04 01:41:43 -0300

    refactor: Improve Cloudinary configuration logging for better visibility

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,10 @@
 import cloudinary
-import cloudinary.uploader
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+print("Configuring Cloudinary...", os.getenv('CLOUDINARY_NAME'), "\n", os.getenv('CLOUDINARY_API_KEY'), "\n", os.getenv('CLOUDINARY_API_SECRET'))
 
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_NAME'),
